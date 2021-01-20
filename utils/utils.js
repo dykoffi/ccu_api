17a966dc9c8e5201e9406d6c1ded3ef159c777ac
dykoffi <dykoffi@gmail.com>
2021-01-20 06:31:21 +0000

    update csv deal

--- utils/utils.js
+++ utils/utils.js
@@ -13,7 +13,6 @@ function updateVCF(folder, fileStream, fileName, indice) {
         file = fileName
         file = file.split(".vcf").join(`(${++i}).vcf`)
     }
-    let timestamp = (new Date()).getTime().toString()
     return new Promise((resolve, reject) => {
         fileStream
             .pipe(new sequenceToLine())
@@ -33,15 +32,49 @@ function updateCSV(folder, fileStream, fileName, indice) {
         file = fileName
         file = file.split(".csv").join(`(${++i}).csv`)
     }
-    return new Promise((resolve, reject) => {
-        fileStream
-            .pipe(new sequenceToLine())
-            .pipe(new cspModifyNumber(indice))
-            .pipe(new csvModifyNumber(indice))
-            .pipe(createWriteStream(join('public/_files', folder, 'ccu-' + file)))
-            .on("finish", () => {
-                resolve(join('public/_files', folder, 'ccu-' + file))
-            })
+    return new Promise(async (resolve, reject) => {
+        const workbook = new ExcelJS.Workbook();
+        await workbook.csv.read(fileStream);
+        const sheet = workbook.getWorksheet()
+        sheet.eachRow((row, i) => {
+            row._cells.forEach(cell => {
+                try {
+                    let value = sheet.getCell(cell.address).value
+                    if (value instanceof Object) {
+                        sheet.getCell(cell.address).value = value
+                    }
+                    else if (value === null) {
+                        sheet.getCell(cell.address).value = null
+                    }
+                    else {
+                        value = value.toString()
+                        if (value.includes('/')) {
+                            let tab = value.split('/')
+                            sheet.getCell(cell.address).value = tab.map(contact => transformContacts(contact, indice)).join(' / ')
+                        } else if (value.includes('\n')) {
+                            let tab = value.split('\n')
+                            tab.forEach((elt, i) => {
+                                if (elt.includes(':') && elt.split(':').length === 2) {
+                                    let tab2 = elt.split(':')
+                                    tab[i] = tab2[0] + " : " + transformContacts(tab2[1], indice)
+                                }
+                            })
+                            sheet.getCell(cell.address).value = tab.join('\n')
+                        }
+                        else if (value.includes(':') && value.split(':').length === 2) {
+                            let tab = value.split(':')
+                            sheet.getCell(cell.address).value = tab[0] + " : " + transformContacts(tab[1], indice)
+                        } else {
+                            sheet.getCell(cell.address).value = transformContacts(value.toString(), indice)
+                        }
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            });
+        })
+        await workbook.csv.writeFile(join('public/_files', folder, 'ccu-' + file));
+        resolve(join('public/_files', folder, 'ccu-' + file))
     })
 }
 
@@ -50,48 +83,41 @@ function updateEXCEL(folder, fileStream, fileName, indice) {
     let i = 0
     while (existsSync(join('public/_files', folder, 'ccu-' + file))) {
         file = fileName
-        file = file.split(".csv").join(`(${++i}).csv`)
+        file = file.split(".xlsx").join(`(${++i}).xlsx`)
     }
     return new Promise(async (resolve, reject) => {
-        const workbookR = new ExcelJS.stream.xlsx.WorkbookReader('./cidliste.xlsx');
         const workbook = new ExcelJS.Workbook();
         await workbook.xlsx.read(fileStream);
         const sheet = workbook.getWorksheet()
-        workbookR.read()
-        workbookR.on('worksheet', async worksheet => {
-            worksheet.on('row', row => {
-                row._cells.forEach(cell => {
-                    try {
-                        let value = sheet.getCell(cell.address).value
-                        if (value instanceof Object) {
-                            sheet.getCell(cell.address).value = value
-                        }
-                        else if (value === null) {
-                            sheet.getCell(cell.address).value = null
-                        }
-                        else {
-                            value = value.toString()
-                            if (value.includes('/')) {
-                                let tab = value.split('/')
-                                sheet.getCell(cell.address).value = tab.map(contact => transformContacts(contact, indice)).join(' / ')
-                            } else if (value.includes(':') && value.split(':').length === 2) {
-                                let tab = value.split(':')
-                                sheet.getCell(cell.address).value = tab[0] + " : " + transformContacts(tab[1], indice)
-                            } else {
-                                sheet.getCell(cell.address).value = transformContacts(value.toString(), indice)
-                            }
-                        }
-                    } catch (error) {
-                        console.log(error);
+        sheet.eachRow((row, i) => {
+            row._cells.forEach(cell => {
+                try {
+                    let value = sheet.getCell(cell.address).value
+                    if (value instanceof Object) {
+                        sheet.getCell(cell.address).value = value
                     }
-                });
-
+                    else if (value === null) {
+                        sheet.getCell(cell.address).value = null
+                    }
+                    else {
+                        value = value.toString()
+                        if (value.includes('/')) {
+                            let tab = value.split('/')
+                            sheet.getCell(cell.address).value = tab.map(contact => transformContacts(contact, indice)).join(' / ')
+                        } else if (value.includes(':') && value.split(':').length === 2) {
+                            let tab = value.split(':')
+                            sheet.getCell(cell.address).value = tab[0] + " : " + transformContacts(tab[1], indice)
+                        } else {
+                            sheet.getCell(cell.address).value = transformContacts(value.toString(), indice)
+                        }
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
             });
-        });
-        workbookR.on('end', async () => {
-            await workbook.xlsx.writeFile(join('public/_files', folder, 'ccu-' + file));
-            resolve(join('public/_files', folder, 'ccu-' + file))
-        });
+        })
+        await workbook.xlsx.writeFile(join('public/_files', folder, 'ccu-' + file));
+        resolve(join('public/_files', folder, 'ccu-' + file))
     })
 }
 function updateZIP(folder, fileStream, fileName, indice) {
@@ -140,7 +166,6 @@ function updateZIP(folder, fileStream, fileName, indice) {
             })
     })
 }
-
 
 exports.updateVCF = updateVCF
 exports.updateCSV = updateCSV
